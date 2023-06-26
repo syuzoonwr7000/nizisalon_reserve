ce873527c2eb6bb75b0672e6e7d465ac2cdabbbc
syuzoonwr7000 <syuzoonwr7000@gmail.com>
2023-06-26 11:06:58 +0900

    jetstream、adminlte導入

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -15,12 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name',100);
-            $table->string('email',100);
-            $table->string('password',100);
-            $table->tinyInteger('role');
+            $table->string('name');
+            $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
             $table->rememberToken();
+            $table->foreignId('current_team_id')->nullable();
+            $table->string('profile_photo_path', 2048)->nullable();
             $table->timestamps();
         });
     }
@@ -34,4 +35,4 @@ class CreateUsersTable extends Migration
     {
         Schema::dropIfExists('users');
     }
-}
+};
--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class UsersTableSeeder extends Seeder
 {
@@ -13,6 +14,22 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        User::insert([
+            'name' => '開発',
+            'email' => 'developer@email.com',
+            'password' => password_hash('12341234', PASSWORD_DEFAULT),
+            'role' => 1,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        
+        User::insert([
+            'name' => 'オーナー',
+            'email' => 'admin@email.com',
+            'password' => password_hash('12341234', PASSWORD_DEFAULT),
+            'role' => 2,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 }
